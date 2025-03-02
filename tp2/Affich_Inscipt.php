747379bbab916c67f638b6bdf5b6d68913dda2e3
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-03-02 12:07:36 +0100

    c4

--- tp2/Affich_Inscipt.php
+++ tp2/Affich_Inscipt.php
@@ -1,10 +1,41 @@
 <?php
-$nom = htmlspecialchars($_POST«["nom"]);
-$prenom = htmlspecialchars($_POST«["prenom"]);
-$classe = htmlspecialchars($_POST«["classe"]);
-$option = htmlspecialchars($_POST«["option"]);
-$sexe = htmlspecialchars($_POST«["sexe"]);
-$jouner = htmlspecialchars($_POST«["journe"]);
 
+if (
+    $_SERVER['REQUEST_METHOD'] == 'POST'
+    && !empty($_POST["nom"])
+    && !empty($_POST["prenom"])
+    && !empty($_POST["classe"])
+    && !empty($_POST["option"])
+    && !empty($_POST["sexe"])
+    && !empty($_POST["journe"])
+) {
+    $nom = htmlspecialchars($_POST["nom"]);
+    $prenom = htmlspecialchars($_POST["prenom"]);
+    $classe = htmlspecialchars($_POST["classe"]);
+    $option = $_POST["option"];
+    $sexe = $_POST["sexe"];
+    $jouner = $_POST["journe"];
+    //open ul
+    echo "<ul>";
 
+    echo "<li> nom : " . $nom . "</li>";
+    echo "<li> prenom : " . $prenom . "</li>";
+    echo "<li> classe : " . $classe . "</li>";
+    echo "<li> sexe : " . $sexe . "</li>";
+    //affiche option
+    echo "<li>options :</li>";
+    foreach ($option as $opt) {
+        echo " -" . $opt;
+    }
+    //affiche journer
+    echo "<li>" . $jouner . "</li>";
+    //close ul
+    echo "</ul>";
+} else { ?>
+<script>
+alert("remplir tous les champs !")
+window.location.replace("index.html")
+</script>
+<?php
+}
 ?>
